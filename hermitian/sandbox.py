f51e5cddfcef7c89d8e4cf7a9e42ae2b85ae03d5
mal <mal@debian-BULLSEYE-live-builder-AMD64>
2021-12-27 15:44:14 -0500

    Added a sandbox file.

---
+++ hermitian/sandbox.py
@@ -0,0 +1,10 @@
+from hermitian import check_phi_gamma_product
+
+
+def main() -> None:
+    """A sandbox for playing around."""
+    check_phi_gamma_product(a=1, b=0, q=(1,))
+
+
+if __name__ == "__main__":
+    main()
